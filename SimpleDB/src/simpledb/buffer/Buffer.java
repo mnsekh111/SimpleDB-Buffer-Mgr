7f3220b32ceb9d44f565131e09c9549bf3967a5b
mnsekh111 <mnsekh111@gmail.com>
2015-11-29 14:11:52 -0500

    Cleanup and reformat

--- SimpleDB/src/simpledb/buffer/Buffer.java
+++ SimpleDB/src/simpledb/buffer/Buffer.java
@@ -19,7 +19,7 @@ public class Buffer {
 	private int modifiedBy = -1; // negative means not modified
 	private int logSequenceNumber = -1; // negative means no corresponding log
 										// record
-	private BufferStats stats = new BufferStats();
+	private BufferStats stats = new BufferStats(); // Holds statistics about this Buffer
 
 	/**
 	 * Creates a new buffer, wrapping a new {@link simpledb.file.Page page}.
